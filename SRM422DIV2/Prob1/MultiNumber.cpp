e7991dde4374d35485d733020f557d95fd05783b
joe.harter <testdrivenjoe@gmail.com>
2013-11-18 19:45:41 -0500

    Solution to MultiNumber

---
+++ SRM422DIV2/Prob1/MultiNumber.cpp
@@ -0,0 +1,38 @@
+#include <math.h>
+#include <string>
+using namespace std;
+
+class MultiNumber
+{
+	public: 
+		string check(int number);
+		int productOfDigits(int number);
+};
+
+string MultiNumber::check(int number)
+{
+	  bool multi = false;
+          for (int i = 1; i < floor(log10(number)) && multi == false; i++)
+          {
+        	  int right = (int) (number % (int) pow(10, i));
+                  int left = (int) ((number - (number % (int) pow(10, i))) / pow(10, i));
+
+	          multi = productOfDigits(left) == productOfDigits(right);
+          }
+
+            return multi ? "YES": "NO";
+}
+
+int MultiNumber::productOfDigits(int number)
+{
+            int product = 1;
+
+            for (int i = floor(log10(number)) + 1; i > 0; i--)
+            {
+                int digit =  (int) (number / (int) pow(10, i - 1));
+                number = (int) (number % (int) pow(10, i - 1));
+                product *= digit;
+            }
+
+            return product;
+}
