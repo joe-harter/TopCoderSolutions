05005978ebf2526133859eb2c9f021077ff05139
joe.harter <testdrivenjoe@gmail.com>
2013-11-21 01:29:59 -0500

    This is a slightly closer solution, but I'm still going to miss when k > 2.

--- SRM422DIV2/Prob3/BirthdayCake.cpp
+++ SRM422DIV2/Prob3/BirthdayCake.cpp
@@ -4,44 +4,60 @@
 #include <assert.h>
 #include <map>
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
 class BirthdayCake
 {
 	public:
 		int howManyFriends(vector<string> availableFruits, vector<string> friendsDislikings, int k);
+                long countBits(long n);
 
 };
 
 int BirthdayCake::howManyFriends(vector<string> availableFruits, vector<string> friendsDislikings, int k)
 {
-    vector<string> test;
-    vector<int> sorted;
-    vector<int>::iterator it;
-    bool found;
-    it = sorted.begin();
-    
     int numFriends = friendsDislikings.size();
+    int numFruits = availableFruits.size();
+    vector<int> sorted;
+    vector<long> friendResultsPerFruit (numFruits, 0);
+    vector<int>::iterator it;
+    it = sorted.begin();
+    bool found;
     
     for(int i = 0; i < availableFruits.size(); i++)
     {
         string fruitName = availableFruits.at(i);
-        int numLikings = numFriends;
+        
         for(int d = 0; d < numFriends; d++)
         {
-            string friendLikings = friendsDislikings.at(d).insert(0, " ");
-            friendLikings.insert(friendLikings.size(), " ");
-            found = friendLikings.find(" " + fruitName + " ") != std::string::npos;
-            numLikings -= found ? 1 : 0;
+            string friendDisLikings = friendsDislikings.at(d).insert(0, " ");
+            friendDisLikings.insert(friendDisLikings.size(), " ");
+            found = friendDisLikings.find(" " + fruitName + " ") != std::string::npos;
+            friendResultsPerFruit.at(i) += found ? 0 : pow(2, d);
         }
-        it = sorted.insert(it, numLikings);
     }
     
-    sort(sorted.begin(), sorted.end());
-    return sorted.at(availableFruits.size() - k);
+    long maxFriends = 0;
+    for(int p = 0; p < friendResultsPerFruit.size(); p++)
+    {
+        for(int s = p+1; s < friendResultsPerFruit.size(); s++)
+        {
+            maxFriends = max(countBits(friendResultsPerFruit.at(p) & friendResultsPerFruit.at(s)), maxFriends);
+        }
+    }
+    
+    return maxFriends;
     
 }
 
+long BirthdayCake::countBits(long n)
+{
+    int c;
+    for (c = 0; n; c++)
+        n &= n - 1;
+    return c;
+}
 
 // For testing purposes only
 int main()
@@ -96,8 +112,39 @@ int main()
     it = friendsDislikings.insert(it, "pineapple");
     it = friendsDislikings.insert(it, "pineapple");
     
-     n = obj.howManyFriends(availableFruits, friendsDislikings, 4);
-      assert(n == 2);
-        cout << "All assertions passed" << "\n";
-        return 0;
+    n = obj.howManyFriends(availableFruits, friendsDislikings, 4);
+    assert(n == 2);
+    
+    // Test 4
+    availableFruits.clear();
+    friendsDislikings.clear();
+    
+    availableFruits.push_back("apple");
+    availableFruits.push_back("orange");
+    availableFruits.push_back("banana");
+    
+    friendsDislikings.push_back("apple orange");
+    friendsDislikings.push_back("apple banana");
+    friendsDislikings.push_back("banana orange");
+    n = obj.howManyFriends(availableFruits, friendsDislikings, 2);
+    cout << n;
+    assert(n == 0);
+    
+        // Test 4
+    availableFruits.clear();
+    friendsDislikings.clear();
+    
+    availableFruits.push_back("apple");
+    availableFruits.push_back("orange");
+    availableFruits.push_back("banana");
+    
+    friendsDislikings.push_back("apple");
+    friendsDislikings.push_back("banana");
+    friendsDislikings.push_back("orange");
+    n = obj.howManyFriends(availableFruits, friendsDislikings, 3);
+    cout << n;
+    assert(n == 0);
+    
+    cout << "All assertions passed" << "\n";
+    return 0;
 }
